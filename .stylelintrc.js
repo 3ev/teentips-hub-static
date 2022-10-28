c45c087d4f040fd5f35b606521f05911a638e4eb
Lewis Nyman <hello@lewisnyman.co.uk>
2022-10-28 12:33:13 +0100

    WHUB-252: Fixed Unexpected unknown at-rule @use and Expected variable to be kebab-case

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -38,6 +38,7 @@ module.exports = {
                     "responsive",
                     "variants",
                     "screen",
+                    "use",
                 ],
             },
         ],
