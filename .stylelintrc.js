97f6157f8568835bbb2b47e8b404ef51025e723c
Lewis Nyman <hello@lewisnyman.co.uk>
2022-10-28 12:18:17 +0100

    WHUB-252: Imported lint config and ran stylelint fix. Removed Prettier for now due to conflicts

---
+++ .stylelintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    extends: [
+        "stylelint-config-standard",
+        "stylelint-config-standard-scss"
+    ],
+    overrides: [
+        {
+            files: ["src/sass/**/*.scss"],
+            customSyntax: "postcss-scss",
+        },
+    ],
+    rules: {
+        indentation: 4,
+        "no-empty-source": null,
+        "string-quotes": "double",
+        "at-rule-no-unknown": [
+            true,
+            {
+                ignoreAtRules: [
+                    "extend",
+                    "at-root",
+                    "debug",
+                    "warn",
+                    "error",
+                    "if",
+                    "else",
+                    "for",
+                    "each",
+                    "while",
+                    "mixin",
+                    "include",
+                    "content",
+                    "return",
+                    "function",
+                    "tailwind",
+                    "apply",
+                    "responsive",
+                    "variants",
+                    "screen",
+                ],
+            },
+        ],
+    },
+}
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const PORT = 3004
 
-app.use(express.static('dist'))
+app.use(express.static("dist"))
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
