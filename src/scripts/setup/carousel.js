fbb950581ceb1fd4f92551a8e835bb52171b5424
Lewis Nyman <hello@lewisnyman.co.uk>
2022-10-31 11:04:47 +0000

    WHUB-252: Boom! Carousel is looking pretty

--- src/scripts/setup/carousel.js
+++ src/scripts/setup/carousel.js
@@ -2,9 +2,12 @@ import 'slick-carousel';
 
 const Carousel = {
     init() {
-        $('.carousel').each(function() {
+        $('.js-carousel').each(function() {
             const $carousel = $(this);
-            $carousel.slick();
+            $carousel.slick({
+                prevArrow: "<button type='button' class='slick-prev carousel__prev'><span class='icon icon-arrowleft'></span><span class='visually-hidden'>Previous</span></button>",
+                nextArrow: "<button type='button' class='slick-next carousel__next'><span class='icon icon-arrowright'></span><span class='visually-hidden'>Next</span></button>"
+              });
         })
         
     },
