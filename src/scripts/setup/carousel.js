c28f7b5d4ccc4a38b1397da889107066cdf58e29
Lewis Nyman <hello@lewisnyman.co.uk>
2022-10-31 11:09:38 +0000

    WHUB-252: Linting

--- src/scripts/setup/carousel.js
+++ src/scripts/setup/carousel.js
@@ -5,8 +5,8 @@ const Carousel = {
         $('.js-carousel').each(function() {
             const $carousel = $(this);
             $carousel.slick({
-                prevArrow: "<button type='button' class='slick-prev carousel__prev'><span class='icon icon-arrowleft'></span><span class='visually-hidden'>Previous</span></button>",
-                nextArrow: "<button type='button' class='slick-next carousel__next'><span class='icon icon-arrowright'></span><span class='visually-hidden'>Next</span></button>"
+                prevArrow: '<button type="button" class="slick-prev carousel__prev"><span class="icon icon-arrowleft"></span><span class="visually-hidden">Previous</span></button>',
+                nextArrow: '<button type="button" class="slick-next carousel__next"><span class="icon icon-arrowright"></span><span class="visually-hidden">Next</span></button>',
               });
         })
         
